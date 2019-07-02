858a3bacb033a661a9e53d5ebc46ecb66f3a4693
Jeremy Ben-Meir <jsb459@cornell.edu>
2019-07-01 23:37:55 -0400

    Initial Commit

---
+++ foli-image/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foli-image
+//
+//  Created by Jeremy Ben-Meir on 7/1/19.
+//  Copyright © 2019 Jeremy Ben-Meir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
